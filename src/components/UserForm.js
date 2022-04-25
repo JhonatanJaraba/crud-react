989ec92375ee3199b8fdd8c4994fae96d0fc3e44
Yonata Jaraba <ygjr_123@hotmail.com>
2022-04-25 12:33:13 -0500

    Subiendo primeros cambios del proyecto

---
+++ src/components/UserForm.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+const UserForm = () =>{
+    return (
+        <form className="card card-body">
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">insert_link</i>
+                </div>
+                <input type="text"
+                className="form-control" 
+                placeholder="https://someurl.com"
+                name="url"/>
+            </div>
+                <br/>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">assignment_ind</i>
+                </div>
+                <input type="text"
+                className="form-control" 
+                placeholder="Ingrese el nombre"
+                name="nombre"/>
+            </div>
+                <br/>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">create</i>
+                </div>
+                <input type="text"
+                className="form-control" 
+                placeholder="Ingrese el apellido"
+                name="apellido"/>
+            </div>
+            <br/>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">call_end</i>
+                </div>
+                <input type="text"
+                className="form-control" 
+                placeholder="Ingrese el telefono"
+                name="telefono"/>
+            </div>
+            <br/>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">keyboard_return</i>
+                </div>
+                <input type="text"
+                className="form-control" 
+                placeholder="Ingrese la direccion"
+                name="direccion"/>
+            </div>
+            <br/>
+            <div className="form-group input-group">
+                <div className="input-group-text bg-light">
+                <i className="material-icons">email</i>
+                </div>
+                <input type="email"
+                className="form-control" 
+                placeholder="Ingrese correo electronico"
+                name="email"/>
+            </div>
+            <br/>
+            <div className="form-group">
+                <textarea name="description" rows="3" className="form-control"
+                placeholder="Write a description">
+                </textarea>
+            </div>
+            <br/>
+            <button className="btn btn-primary btn-block">
+                save
+            </button>
+        </form>
+    )
+}
+
+export default UserForm;
