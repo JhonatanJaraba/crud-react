c14939a1b63421e4955e2591d133afb5a54ed098
Yonata Jaraba <ygjr_123@hotmail.com>
2022-04-25 12:39:44 -0500

    Cambios al evento

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const UserForm = () =>{
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        console.log(e);
+    }
+
     return (
-        <form className="card card-body">
+        <form className="card card-body" onSubmit={handleSubmit}>
             <div className="form-group input-group">
                 <div className="input-group-text bg-light">
                 <i className="material-icons">insert_link</i>
